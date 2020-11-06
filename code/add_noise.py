4d77d2101e39b857468505d84a7b8652ac1d725c
Jason Zhou <zhoujc999@gmail.com>
2020-11-06 17:00:08 -0500

    Add add_noise.py

---
+++ code/add_noise.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+import sys
+import random
+import csv
+
+rows = list()
+with open(sys.argv[1], 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    rows = list(csv_reader)
+
+data = rows[1:]
+
+time, ra_data, dec_data = zip(*data)
+
+ra_in_sec = list()
+for ra in ra_data:
+    hour = int(ra[0:2])
+    minute = int(ra[3:5])
+    sec = float(ra[6:])
+    ra_in_sec.append(hour * 60 * 60 + minute * 60 + sec)
+
+average_ra_diff = 0
+for i in range(len(ra_in_sec) - 1):
+    average_ra_diff += ra_in_sec[i + 1] - ra_in_sec[i]
+average_ra_diff /= (len(ra_in_sec) - 1)
+
+
+sigma_ra = average_ra_diff / 2
+
+noisy_ra_in_sec = list()
+for ra in ra_in_sec:
+    noisy_ra = round(ra + random.gauss(0, sigma_ra), 2)
+    if noisy_ra < 0:
+        noisy_ra_in_sec.append(24 * 60 * 60 + noisy_ra)
+    elif noisy_ra >= 24 * 60 * 60:
+        noisy_ra_in_sec.append(noisy_ra - 24 * 60 * 60)
+    else:
+        noisy_ra_in_sec.append(noisy_ra)
+
+
+dec_in_arcsec = list()
+for dec in dec_data:
+    positive = dec[0] == '+'
+    degree = int(dec[1:3])
+    minute = int(dec[4:6])
+    sec = float(dec[7:])
+    if positive:
+        dec_in_arcsec.append(degree * 3600 + minute * 60 + sec)
+    else:
+        dec_in_arcsec.append(-1 * (degree * 3600 + minute * 60 + sec))
+
+average_dec_diff = 0
+for i in range(len(dec_in_arcsec) - 1):
+    average_dec_diff += dec_in_arcsec[i + 1] - dec_in_arcsec[i]
+average_dec_diff /= (len(dec_in_arcsec) - 1)
+
+
+sigma_dec = average_dec_diff / 2
+
+noisy_dec_in_arcsec = list()
+for dec in dec_in_arcsec:
+    noisy_dec = round(dec + random.gauss(0, sigma_dec), 1)
+    noisy_dec_in_arcsec.append(noisy_dec)
+
+
+csv_list = zip(time, noisy_ra_in_sec, noisy_dec_in_arcsec)
+
+
+# Set the output csv file name
+outputFileName = sys.path[0] + '/' + time[0][0:4] + '.csv'
+
+# Write the output list in csv format to a new file
+with open(outputFileName, mode='w') as outputFile:
+    writer = csv.writer(outputFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['Date', 'Right ascension (in seconds)', 'Declination (in arc seconds)'])
+    writer.writerows(csv_list)
+
+print('Output file name: ' + outputFileName)
